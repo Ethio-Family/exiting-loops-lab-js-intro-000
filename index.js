12c19f6842d693d24c2378fb3949a5f6bdc07b83
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-03 02:43:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ function breakOut(array, changeValue, stopValue) {
 
   return array;
 }
+
+// keepGoing(array, changeValue, skipValue) which iterates through array
+// and changes every element to changeValue
+// except those that match skipValue. Then return the array.
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] == skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
