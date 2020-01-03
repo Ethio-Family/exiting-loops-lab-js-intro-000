bdc7b4bd30b18ac1069f88e5e656a145c95be7b5
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-03 02:38:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,4 @@
+// breakOut(array, changeValue, stopValue) which iterates through array
+// and changes every element to changeValue until the loop reaches stopValue.
+// Then we break out of the loop and return the array.
+
